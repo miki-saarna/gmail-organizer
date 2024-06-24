659461bb49de5f7f7c9d67ffc0cb42ca9d016ebd
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-06-23 19:50:58 -0700

    Refactored options selection

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -10,7 +10,14 @@ import (
 	"os"
 )
 
+const (
+	deletion = "Delete emails"
+	updateSpam =  "Update SPAM list"
+	exit =  "Exit"
+)
+
 type deletionList []string
+
 
 func main () {
 	var deletionList deletionList
@@ -35,23 +42,27 @@ func main () {
 	}
 	// fmt.Printf("unmarshalled: %v", deletionList)
 
-	options := utils.Options{"Delete emails", "Exit"}
+	options := utils.Options{deletion, updateSpam, exit}
 	selectedOption, err := options.SelectOption()
 	if err != nil {
 		log.Fatalf("There was an error selecting an option: %v", err)
 	}
 
-	if selectedOption == options[0] {
+	if selectedOption == deletion {
 		var senderBulletPointList string
 		for _, senderAddress := range deletionList {
 			senderBulletPointList += fmt.Sprintf("\n- %v", senderAddress)
 		}
 
 		confirmationMsg := utils.ConfirmationMsg(fmt.Sprintf("Are you sure you would like to permanently delete all emails from the following senders: %v?", senderBulletPointList))
-		isConfirmed := confirmationMsg.AskForConfirmation()
-		if (isConfirmed) {
+		isConfirmed, err := confirmationMsg.AskForConfirmation()
+		if err != nil {
+			log.Fatalf("There was an error selection an option: %v", err)
+		} else if (isConfirmed) {
 			gmail.Main(deletionList);
 		}
+	} else if selectedOption == updateSpam {
+		// create update SPAM list func
 	}
 
 	// if len(os.Args) < 2 {
